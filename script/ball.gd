08e68e1ea96b14d5799d611a570da56cd748057f
Mew789 <135380086+CarlosOT2@users.noreply.github.com>
2026-09-05 16:55:02 -0300

    a

--- script/ball.gd
+++ script/ball.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 var direction: Vector2 = Vector2(-1, -1).normalized()
 var speed: float = 500
 
+@onready var hitsound: AudioStreamPlayer2D = $hitsound
+
 func _physics_process(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
 	
 	if get_last_slide_collision() != null:
+		
 		var normal = get_last_slide_collision().get_normal()
 		direction = direction.bounce(normal)
+		hitsound.play()
--- script/main.gd
+++ script/main.gd
@@ -4,17 +4,21 @@ var scorePlayer = 0
 var scoreBot = 0
 
 @onready var score_label: Label = $CanvasLayer/ScoreLabel
+@onready var bot_score_sound: AudioStreamPlayer = $botScoreSound
+@onready var player_score_sound: AudioStreamPlayer = $playerScoreSound
 
 func _on_goal_1_body_entered(body: Node2D) -> void:
 	body.global_position = Vector2i(566, 332)
 	scoreBot += 1
 	updateScore()
+	bot_score_sound.play()
 
 
 func _on_goal_2_body_entered(body: Node2D) -> void:
 	body.global_position = Vector2i(566, 332) 
 	scorePlayer += 1
 	updateScore()
+	player_score_sound.play()
 
 func updateScore():
 	score_label.text = str(scorePlayer) + " x " + str(scoreBot)
